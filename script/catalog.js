5a1de6c02154e32ce6ef63adec2c5b9d0a18888c
Marlo <marlo.baguyos@outlook.com>
2020-04-21 12:43:53 -0400

    update

--- script/catalog.js
+++ script/catalog.js
@@ -3,6 +3,12 @@ var items = [];
 
 function register() {
     console.log("Current items" + items.length);
+    var code = $("#code").val();
+    var title = $("#title").val();
+    var price = $("#price").val();
+
+
+
 }
 
 register();
