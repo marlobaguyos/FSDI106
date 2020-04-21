3bffcdcd90ed72d89cc6a5ae8fc1635d84d2aeef
Marlo <marlo.baguyos@outlook.com>
2020-04-21 14:05:39 -0400

    udpate

--- script/catalog.js
+++ script/catalog.js
@@ -6,12 +6,23 @@ function register() {
     var code = $("#code").val();
     var title = $("#title").val();
     var price = $("#price").val();
-    var description = $("#description");
-    var category = $("#category");
-    var image = $("#image");
+    var description = $("#description").val();
+    var category = $("#category").val();
+    var image = $("#image").val();
 
+    items.push({
+        code:code,
+        title:title,
+        price:price,
+        description:description,
+        category:category,
+        image:image
+    });
 
+    console.log("New item: " + items.length);
 
 }
 
-register();
+$("#register-btn").on('click', function(){
+    register();
+});
