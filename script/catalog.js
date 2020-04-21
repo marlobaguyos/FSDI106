c070b817861501851151789efd2e41f72c68a026
Marlo <marlo.baguyos@outlook.com>
2020-04-21 15:22:57 -0400

    update

--- script/catalog.js
+++ script/catalog.js
@@ -1,28 +1,31 @@
 /* global variables */
-var items = [];
 
-function register() {
-    console.log("Current items" + items.length);
-    var code=$("#code").val();
-    var title=$("#title").val();
-    var price=$("#price").val();
-    var description=$("#description").val();
-    var category=$("#category").val();
-    var image=$("#image").val();
+// var items = [];
 
-    items.push({
-        code:code,
-        title:title,
-        price:price,
-        description:description,
-        category:category,
-        image:image
-    });
+// function register() {
+//     console.log("Current items" + items.length);
+//     var code=$("#code").val();
+//     var title=$("#title").val();
+//     var price=$("#price").val();
+//     var description=$("#description").val();
+//     var category=$("#category").val();
+//     var image=$("#image").val();
 
-    console.log("New item:" + items.length);
+//     items.push({
+//         code:code,
+//         title:title,
+//         price:price,
+//         description:description,
+//         category:category,
+//         image:image
+//     });
 
-}
+//     console.log("New item:" + items.length);
 
-$("#register-btn").on('click', function(){
-    register();
-});
+// }
+
+// $("#register-btn").on('click', function(){
+//     register();
+// });
+
+var items=[];
