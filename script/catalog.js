843fcbbe7c30354591d42757e58bea02eb335ee8
Marlo <marlo.baguyos@outlook.com>
2020-04-22 10:48:03 -0400

    update

--- script/catalog.js
+++ script/catalog.js
@@ -4,7 +4,7 @@ var items = [
     {
         // first item
         code:'1tvs',
-        title:'TV samsung',
+        title:'TV',
         price:1000,
         description:"This is the long description of the item",
         category:'Electronics',
@@ -13,7 +13,7 @@ var items = [
     {
         //second item
         code: '1ph10',
-        title: 'Iphone X',
+        title: 'Phone',
         price: 10000,
         description: "This is the long description of the item",
         category: 'Mobile devices',
@@ -106,17 +106,18 @@ $('#search-btn').on('click', function(){
     var searchString = $('#search-txt').val();
     /* travel the array */
     for(var i=0;i<items.length;i++){
+        /* conditional */
         if(searchString.toUpperCase() != items[i].title.toUpperCase()){
+            /* execute the change */
             $('#' + items[i].code).hide();
         }
         else {
             $('#' + items[i].code).show();
         }
     }
-    /* conditional */
-
-    /* execute the change */
-})
+    
+    
+});
 
 
 /* initialization */
