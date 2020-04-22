a1737e976e3cb8b4fbf0e82a188eb1152a1a9615
Marlo <marlo.baguyos@outlook.com>
2020-04-22 09:52:16 -0400

    update

--- script/catalog.js
+++ script/catalog.js
@@ -53,7 +53,7 @@ function displayCatalog(){
             <h4>${product.title}</h4>
             <h6>${product.price}</h6>
             <p>${product.description}</p>
-            <div class"button-div">
+            <div class="button-div">
                 <button class="btn btn-primary mb-2">Add to Cart</button>
             </div>
             
