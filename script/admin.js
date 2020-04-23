d67132028a16e5862dc731b09c6f116da2d4e8be
Marlo <marlo.baguyos@outlook.com>
2020-04-22 22:17:59 -0400

    update

--- script/admin.js
+++ script/admin.js
@@ -1,3 +1,5 @@
+var serverURL=""http://restclass.azurewebsites;
+
 var items=[];
 
 function init(){
@@ -5,6 +7,19 @@ function init(){
 }
 
 window.onload=init;
+
+//object constructor
+
+class Item {
+    constructor(code, title, price, description, category, image) {
+        this.code=code;
+        this.title=title;
+        this.price=price;
+        this.description=description;
+        this.category=category;
+        this.image=image;
+    }
+}
 
 function register() {
     //save from the input in a var
@@ -16,21 +31,71 @@ function register() {
     var category=$('#category').val();
     var image=$('#image').val();
 
-    //assing the var to the attribute
-    items.push({
-        code:code,
-        title:title,
-        price:price,
-        description:description,
-        category:category,
-        image:image
-    });
+    if(code!=" " && title!=" " && price!=" " && description!=" " && category!=" " && image!=" " ){
+        
+// create an object
+    var newItem=new Item(code,title,price,description,category,image);
+    // assing the var to the attribute
+    items.push(newItem);
+    console.log(newItem);
 
     console.log('New item:' + items.length);
-    alert("You register a new product")
+    alert("You registered a new product")
+    }
 
 }
 
 $("#register-btn").on('click', function(){
     register();
 });
+
+// homework
+
+function solveHW(){
+    var data=[
+        {
+            age:28,
+            name:"Eli",
+            color:"Orange"
+        },
+        {
+            age:35,
+            name:"Zach",
+            color:"Blue",
+        },
+        {
+            age:26,
+            name:"Larry",
+            color:"Blue"
+        },
+        {
+            age:37,
+            name:"Ed",
+            color:"blue"
+        },
+        {
+            age:30,
+            name:"Jeremy",
+            color:"Peach",
+        },
+        {
+            age:28,
+            name:"Pavel",
+            color:"purple"
+        },
+        {
+            age:33,
+            name:"Chad",
+            color:"Red"
+        }
+    ]
+}
+
+//Who (name - age) is the oldest
+
+// for(var i; i < data.length; i++){
+
+// }
+
+
+// read about HTTP methods (GET, POST, PUT, PATCH, DELETE)
