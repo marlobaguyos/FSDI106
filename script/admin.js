52117fda3eebc40f1bbfec0a4134f9c4adf9a404
Marlo <marlo.baguyos@outlook.com>
2020-04-28 20:01:04 -0400

    updat

--- script/admin.js
+++ script/admin.js
@@ -1,28 +1,28 @@
-var serverURL="http://localhost:8080/api/";
+var serverURL = "http://localhost:8080/api/";
 
-var items=[];
+var items = [];
 
-function init(){
+function init() {
     console.log("Admin Page");
 }
 
-window.onload=init;
+window.onload = init;
 
 //object constructor
 
 class Item {
     constructor(code, title, price, description, category, image) {
-        this.code=code;
-        this.title=title;
-        this.price=price;
-        this.description=description;
-        this.category=category;
-        this.image=image;
-        this.user="Marlo";
+        this.code = code;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.image = image;
+        this.user = "Marlo";
     }
 }
 
-function clearForm(){
+function clearForm() {
     $('#code').val("");
     $('#code').focus();
     $('#title').val("");
@@ -37,20 +37,20 @@ function clearForm(){
 function register() {
     //save from the input in a var
     console.log('Current items' + items.length);
-    var code=$('#code').val();
-    var title=$('#title').val();
-    var price=$('#price').val();
-    var description=$('#description').val();
-    var category=$('#category').val();
-    var image=$('#image').val();
+    var code = $('#code').val();
+    var title = $('#title').val();
+    var price = $('#price').val();
+    var description = $('#description').val();
+    var category = $('#category').val();
+    var image = $('#image').val();
 
-    if(code!=" " && title!=" " && price!=" " && description!=" " && category!=" " && image!=" " ){
+    if (code != " " && title != " " && price != " " && description != " " && category != " " && image != " ") {
         // create an object
-        var newItem=new Item(code,title,price,description,category,image);
+        var newItem = new Item(code, title, price, description, category, image);
         // assing the var to the attribute
         items.push(newItem);
         //parse the element into a JSON string
-        var jsonString=JSON.stringify(newItem);
+        var jsonString = JSON.stringify(newItem);
 
         console.log(newItem);
         console.log(jsonString);
@@ -59,28 +59,28 @@ function register() {
     //Asyn JS and XML
     //send obj to server (string, int, boolean)
     $.ajax({
-        url:serverURL+"items",
-        type:"POST",
-        contentType:"application/json",
-        data:jsonString,
-        success: function(response){
+        url: serverURL + "items",
+        type: "POST",
+        contentType: "application/json",
+        data: jsonString,
+        success: function (response) {
             console.log("it works", response);
             //show the notification
             $('#alert-box').removeClass("hidden");
             //hide the
-            setTimeout(function(){
+            setTimeout(function () {
                 $('#alert-box').addClass('hidden');
-            },3000);
+            }, 3000);
             clearForm();
         },
-        error:function(errorDetails){
+        error: function (errorDetails) {
             console.log("Error, Something went wrong", errorDetails);
         }
     });
 
 }
 
-$("#register-btn").on('click', function(){
+$("#register-btn").on('click', function () {
     register();
 });
 
